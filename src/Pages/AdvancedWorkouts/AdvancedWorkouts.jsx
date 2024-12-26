713e62032567724677eba9932b71a89ef2eb9829
Mohan Sai <mohansaichinthala9121@gmail.com>
2024-12-27 02:06:12 +0530

    workout plans pages are created

--- src/Pages/AdvancedWorkouts/AdvancedWorkouts.jsx
+++ src/Pages/AdvancedWorkouts/AdvancedWorkouts.jsx
@@ -5,6 +5,19 @@ import logo from "../../assets/Images/gymfluencer-logo.png";
 import { FaAngleDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import bg from "./images/background.jpg";
+import workout1 from "./images/weightedpullups.jpg";
+import workout2 from "./images/benchpress.jpg";
+import workout3 from "./images/benchpress2.webp";
+import workout4 from "./images/barbellsquats.jpg";
+import workout5 from "./images/deadlift.jpg";
+import workout6 from "./images/bulgariansplit.jpg";
+import workout7 from "./images/battleropes.jpg";
+import workout8 from "./images/benchpress.jpg";
+import workout9 from "./images/kettlebell.jpg";
+import workout10 from "./images/legraises.webp";
+import workout11 from "./images/russiantwists.jpg";
+import workout12 from "./images/bicyclecrunches.jpg";
 
 const IntermediateWorkouts = () => {
   const [isOptions, setIsOptions] = useState(false);
@@ -88,7 +101,9 @@ const IntermediateWorkouts = () => {
         results. Stay consistent to reach your peak fitness potential.
       </div>
       <div className="advanced-workouts-plan-container">
-        <div className="advanced-workouts-plan-bgimg"></div>
+        <div className="advanced-workouts-plan-bgimg">
+          <img src={bg} alt="background img" className="advanced-background" />
+        </div>
         <div className="advanced-workouts-plan-box">
           <div className="advanced-workouts-plan-goal">Goal</div>
           <div className="advanced-workouts-plan-goal-subtitle">
@@ -102,33 +117,168 @@ const IntermediateWorkouts = () => {
           <div className="advanced-workouts-plan-day1">
             Day 1: Upper Body Strength
           </div>
-          <div className="advanced-workouts-plan-day1-img1"></div>
-          <div className="advanced-workouts-plan-day1-img2"></div>
-          <div className="advanced-workouts-plan-day1-img3"></div>
-          <div className="advanced-workouts-plan-day1-matter-container"></div>
+          <div className="advanced-workouts-plan-day1-img1">
+            <img
+              src={workout1}
+              alt="weighted pullups img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">Weighted Pullups</div>
+          </div>
+          <div className="advanced-workouts-plan-day1-img2">
+            <img
+              src={workout2}
+              alt="bench press img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">Bench Press</div>
+          </div>
+          <div className="advanced-workouts-plan-day1-img3">
+            <img
+              src={workout3}
+              alt="overhead shoulder img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">OverHead Shoulder</div>
+          </div>
+          <div className="advanced-workouts-plan-day1-matter-container">
+            <div>Warm-up: 10 mins dynamic stretching</div>
+            <ul>
+              <li>Weighted Pull-Ups: 5 sets of 8 reps</li>
+              <br />
+              <li>Bench Press: 4 sets of 8 reps</li>
+              <br />
+              <li>Overhead Shoulder Press: 4 sets of 10 reps</li>
+              <br />
+              <li>Tricep Dips: 4 sets of 12 reps</li>
+            </ul>
+          </div>
           <div className="advanced-workouts-plan-day2">
             Day 2: Lower Body & Core
           </div>
-          <div className="advanced-workouts-plan-day2-img1"></div>
-          <div className="advanced-workouts-plan-day2-img2"></div>
-          <div className="advanced-workouts-plan-day2-img3"></div>
-          <div className="advanced-workouts-plan-day2-matter-container"></div>
+          <div className="advanced-workouts-plan-day2-img1">
+            <img
+              src={workout4}
+              alt="Barbell squats img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">Barbell Squats</div>
+          </div>
+          <div className="advanced-workouts-plan-day2-img2">
+            <img
+              src={workout5}
+              alt="Dead lifts img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">Dead Lift</div>
+          </div>
+          <div className="advanced-workouts-plan-day2-img3">
+            <img
+              src={workout6}
+              alt="Bulgarian split img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">Bulgarian Split</div>
+          </div>
+          <div className="advanced-workouts-plan-day2-matter-container">
+            <div>Warm-up: 10 mins of light jogging</div>
+            <ul>
+              <li>Barbell Squats: 5 sets of 6 reps</li>
+              <br />
+              <li>Deadlifts: 5 sets of 5 reps</li>
+              <br />
+              <li>Bulgarian Split Squats: 4 sets of 8 reps per leg</li>
+              <br />
+              <li>Calf Raises: 4 sets of 15 reps</li>
+            </ul>
+          </div>
           <div className="advanced-workouts-plan-day3">
             Day 3: Functional Circuit
           </div>
-          <div className="advanced-workouts-plan-day3-img1"></div>
-          <div className="advanced-workouts-plan-day3-img2"></div>
-          <div className="advanced-workouts-plan-day3-img3"></div>
-          <div className="advanced-workouts-plan-day3-matter-container"></div>
+          <div className="advanced-workouts-plan-day3-img1">
+            <img
+              src={workout7}
+              alt="Battle ropes img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">Battles Ropes</div>
+          </div>
+          <div className="advanced-workouts-plan-day3-img2">
+            <img
+              src={workout8}
+              alt="Bench press img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">Bench Press</div>
+          </div>
+          <div className="advanced-workouts-plan-day3-img3">
+            <img
+              src={workout9}
+              alt="Kettlebell swing img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">Kettlebell Swing</div>
+          </div>
+          <div className="advanced-workouts-plan-day3-matter-container">
+            <div>Warm-up: 5 mins jump rope</div>
+            <ul>
+              <li>Battle Ropes: 4 sets of 45 seconds</li>
+              <br />
+              <li>Box Jumps: 4 sets of 15 reps</li>
+              <br />
+              <li>Kettlebell Swings: 4 sets of 20 reps</li>
+              <br />
+              <li>Burpees: 3 sets of 20 reps</li>
+            </ul>
+          </div>
           <div className="advanced-workouts-plan-day4">
             Day 4: Core & Conditioning
           </div>
-          <div className="advanced-workouts-plan-day4-img1"></div>
-          <div className="advanced-workouts-plan-day4-img2"></div>
-          <div className="advanced-workouts-plan-day4-img3"></div>
-          <div className="advanced-workouts-plan-day4-matter-container"></div>
+          <div className="advanced-workouts-plan-day4-img1">
+            <img
+              src={workout10}
+              alt="Hanging leg raises img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">Hanging Leg Raise</div>
+          </div>
+          <div className="advanced-workouts-plan-day4-img2">
+            <img
+              src={workout11}
+              alt="Russian twists img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">Russian Twists</div>
+          </div>
+          <div className="advanced-workouts-plan-day4-img3">
+            <img
+              src={workout12}
+              alt="Bicycle crunches img"
+              className="advanced-workouts-org-img"
+            />
+            <div className="advanced-img-title">Bicycle crunches</div>
+          </div>
+          <div className="advanced-workouts-plan-day4-matter-container">
+            <div>Hanging Leg Raises: 4 sets of 12 reps</div>
+            <ul>
+              <li>Russian Twists: 4 sets of 20 reps</li>
+              <br />
+              <li>Bicycle Crunches: 4 sets of 20 reps</li>
+              <br />
+              <li>Plank: 4 sets of 1 minute</li>
+            </ul>
+          </div>
           <div className="advanced-workouts-plan-day5">Cool-down:</div>
-          <div className="advanced-workouts-plan-day5-matter-container"></div>
+          <div className="advanced-workouts-plan-day5-matter-container">
+            <div>Quad Stretch: 3 sets of 30 seconds per leg</div>
+            <ul>
+              <li>Hamstring Stretch: 3 sets of 30 seconds per leg</li>
+              <br />
+              <li>Seated Forward Bend: 3 sets of 30 seconds</li>
+              <br />
+              <li>Cat-Cow Stretch: 3 sets of 30 seconds</li>
+            </ul>
+          </div>
         </div>
       </div>
       <Support3 />
--- src/Pages/PersonalizedWorkout/PersonalizedWorkout.jsx
+++ src/Pages/PersonalizedWorkout/PersonalizedWorkout.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { FaAngleDown } from "react-icons/fa";
 import logo from "../../assets/Images/gymfluencer-logo.png";
 import { useState } from "react";
+import bg from "./background.jpg";
 
 const PersonalizedWorkout = () => {
   const [isOptions, setIsOptions] = useState(false);
@@ -82,25 +83,172 @@ const PersonalizedWorkout = () => {
         </AnchorLink>
       </div>
       <div className="personalized-workout-container">
-        <div className="personalized-workout-bgimg"></div>
+        <div className="personalized-workout-bgimg">
+          <img src={bg} alt="Background img" />
+        </div>
         <div className="personalized-workout-form-container">
           <div className="personalized-workout-form-container-heading">
             Personalized Workout Plan Form
           </div>
           <div className="personalized-workout-form-container-head1">
             1. Fitness Goals
+            <div className="head1-subhead1">Primary Goals:</div>
+            <div className="left-checkboxes1">
+              <div className="checkbox1">
+                <input type="checkbox" className="check-style-box" />
+                <label>Weight Loss</label>
+              </div>
+              <div className="checkbox2">
+                <input type="checkbox" className="check-style-box" />
+                <label>Endurance</label>
+              </div>
+              <div className="checkbox3">
+                <input type="checkbox" className="check-style-box" />
+                <label>Overall Fitness</label>
+              </div>
+            </div>
+            <div className="right-checkboxes1">
+              <div className="checkbox4">
+                <input type="checkbox" className="check-style-box" />
+                <label>Muscle Gain</label>
+              </div>
+              <div className="checkbox5">
+                <input type="checkbox" className="check-style-box" />
+                <label>Flexibility</label>
+              </div>
+            </div>
+            <div className="head1-subhead2">Areas of Focus:</div>
+            <div className="left-checkboxes2">
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Upper Body</label>
+              </div>
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Core</label>
+              </div>
+            </div>
+            <div className="right-checkboxes2">
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Upper Body</label>
+              </div>
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Core</label>
+              </div>
+            </div>
           </div>
           <div className="personalized-workout-form-container-head2">
             2. Current Fitness Level
+            <div className="head2-title">Fitness Level:</div>
+            <div className="head2-dropdown">
+              <select className="dropdown-options">
+                <option>Beginner</option>
+                <option>Intermediate</option>
+                <option>Advanced</option>
+              </select>
+            </div>
           </div>
           <div className="personalized-workout-form-container-head3">
             3. Physical Measurements and Health Info
+            <div className="head3-part1">
+              <div className="head3-part1-p">
+                <label>Age</label>
+                <input type="text" className="age-input" />
+                <label>Height</label>
+                <input type="text" className="height-input" />
+              </div>
+              <div className="head3-part1-p">
+                <label>Gender</label>
+                <select className="gender-selection">
+                  <option>Male</option>
+                  <option>Female</option>
+                  <option>Other</option>
+                </select>
+                <label>Weight</label>
+                <input type="text" className="weight-input" />
+              </div>
+            </div>
+            <div className="head3-part2">
+              <label>
+                <pre>Injuries or Physical Limitations:</pre>
+              </label>
+              <input type="textarea" className="injuries-input" />
+              <label>
+                <pre>Medical Conditions:</pre>
+              </label>
+              <input type="text" className="condition-input" />
+            </div>
           </div>
           <div className="personalized-workout-form-container-head4">
             4. Workout Preferences
+            <div className="left-checkboxes4">
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Strength Training</label>
+              </div>
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Yoga</label>
+              </div>
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Body Weight</label>
+              </div>
+            </div>
+            <div className="right-checkboxes4">
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Cardio</label>
+              </div>
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>HIIT</label>
+              </div>
+            </div>
           </div>
           <div className="personalized-workout-form-container-head5">
             5. Schedule and Time Availability
+            <div className="left-checkboxes5">
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Sunday</label>
+              </div>
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Monday</label>
+              </div>
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Tuesday</label>
+              </div>
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Wednesday</label>
+              </div>
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Thursday</label>
+              </div>
+            </div>
+            <div className="right-checkboxes5">
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Friday</label>
+              </div>
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Saturday</label>
+              </div>
+              <div>
+                <input type="checkbox" className="check-style-box" />
+                <label>Sunday</label>
+              </div>
+            </div>
+          </div>
+          <div className="submit-button">
+            <button type="submit">Submit</button>
           </div>
         </div>
       </div>
